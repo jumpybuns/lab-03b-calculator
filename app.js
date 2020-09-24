09430ff5659c3b45ee37671c1d6fd80a0d525409
jumpybuns <ethanpiercepresents@gmail.com>
2020-09-24 11:35:49 -0700

    commit it

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// import functions and grab DOM elements
+
 const addInput1 = document.getElementById('add-input-1');
 
-console.log(addInput1.value);
+
 
 const addInput2 = document.getElementById('add-input-2');
 
@@ -16,11 +16,12 @@ const addResults = document.getElementById('add-results');
 console.log(addResults);
 
 
-// set event listeners to update state and DOM
+
 addButton.addEventListener('click', () => {
     const value1 = addInput1.value;
     const value2 = addInput2.value;
 
+    
     const value1AsNumber = Number(value1);
     const value2AsNumber = Number(value2);
     
@@ -29,5 +30,83 @@ addButton.addEventListener('click', () => {
 
     addResults.textContent = sum;
 
+});
+
+
+
+
+
+const subInput1 = document.getElementById('sub-input-1');
+
+
+
+const subInput2 = document.getElementById('sub-input-2');
+
+console.log(subInput2.value);
+
+const subButton = document.getElementById('sub-button');
+
+console.log(addButton);
+
+const subResults = document.getElementById('sub-results');
+
+console.log(subResults);
+
+
+
+subButton.addEventListener('click', () => {
+    const value1 = subInput1.value;
+    const value2 = subInput2.value;
+
+    
+    const value1AsNumber = Number(value1);
+    const value2AsNumber = Number(value2);
+    
+    
+    const subtract = value1AsNumber - value2AsNumber;
+
+    subResults.textContent = subtract;
+
+
+});
+
+const multInput1 = document.getElementById('mult-input-1');
+
+const multInput2 = document.getElementById('mult-input-2');
+
+const multButton = document.getElementById('mult-button');
+
+const multResults = document.getElementById('mult-results');
+
+
+multButton.addEventListener('click', () => {
+    const value1 = multInput1.value;
+    const value2 = multInput2.value;
+
+    const multiply = value1 * value2;
+
+    multResults.textContent = multiply;
+
+
+});
+
+
+
+const divInput1 = document.getElementById('div-input-1');
+
+const divInput2 = document.getElementById('div-input-2');
+
+const divButton = document.getElementById('div-button');
+
+const divResults = document.getElementById('div-results');
+
+
+divButton.addEventListener('click', () => {
+    const value1 = divInput1.value;
+    const value2 = divInput2.value;
+
+    const divide = value1 / value2;
+
+    divResults.textContent = divide;
 })
 
